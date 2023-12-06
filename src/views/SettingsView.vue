a2a79642dee97490cd28fc8d239e17849ad12a1d
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-12-06 20:02:34 +0100

    Correct title "Shelf" to "Settings"

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="title">Shelf</div>
+    <div class="title">Settings</div>
     <div class="q-mt-xl text-center">
       <div class="q-mt-xl q-pa-md about-box text-center">
         There are no settings available yet.
