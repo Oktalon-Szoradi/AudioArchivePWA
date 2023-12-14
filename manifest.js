27d454194c36b324ea1ca20ff24ce50dbf870c64
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2023-12-14 14:23:05 +0100

    PWA/SW basics + renaming

---
+++ manifest.js
@@ -0,0 +1,14 @@
+export default {
+  name: 'AudioArchive',
+  short_name: 'AudioArchive',
+  start_url: '/',
+  display: 'standalone',
+  background_color: '#ffffff',
+  lang: 'en',
+  scope: '/',
+  theme_color: '#ffffff',
+  id: '/',
+  icons: [
+    { src: '/AudioArchive_Logo.svg', type: 'image/svg+xml' }
+  ]
+}
