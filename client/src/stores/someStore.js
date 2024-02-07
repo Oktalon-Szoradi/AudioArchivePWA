e8832bf6bfaa31b5ad76ecc8b0bbec5d8ac595ac
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-02-07 18:42:59 +0100

    Get data from server

--- client/src/stores/someStore.js
+++ client/src/stores/someStore.js
@@ -2,67 +2,15 @@
 
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
+import axios from 'axios'
 
 const useSomeStore = defineStore('SomeStore', () => {
   const pwaTitle = ref('AudioArchive (prototype)')
   const audios = ref([])
 
   const fetchAudios = async () => {
-    audios.value = [
-      {
-        aid: 0,
-        name: 'birds',
-        description: 'birds chirping',
-        timestamp: '2023-11-12T06:16:21Z',
-        rating: 5,
-        audio: ['binary data']
-      },
-
-      {
-        aid: 1,
-        name: 'drink',
-        description: 'me drinking a can of soda',
-        timestamp: '2023-11-13T17:16:05Z',
-        rating: 4,
-        audio: ['binary data']
-      },
-
-      {
-        aid: 2,
-        name: 'laughing',
-        description: 'Syyab laughing',
-        timestamp: '2023-11-14T10:25:43Z',
-        rating: 5,
-        audio: ['binary data']
-      },
-
-      {
-        aid: 3,
-        name: 'television',
-        description: 'CRT turning off',
-        timestamp: '2023-11-15T21:02:21Z',
-        rating: 4,
-        audio: ['binary data']
-      },
-
-      {
-        aid: 4,
-        name: 'dishwasher',
-        description: 'the dishwasher starting',
-        timestamp: '2023-11-16T18:16:21Z',
-        rating: 3,
-        audio: ['binary data']
-      },
-
-      {
-        aid: 5,
-        name: 'song',
-        description: 'me singing',
-        timestamp: '2023-11-17T00:18:21Z',
-        rating: 1,
-        audio: ['binary data']
-      }
-    ]
+    const response = await axios.get('/audioarchive')
+    audios.value = response.data
   }
 
   return { pwaTitle, fetchAudios, audios }
--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,10 @@ import manifest from './manifest.js'
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    port: 8080
+    port: 8080,
+    proxy: {
+      '/audioarchive': 'http://localhost:3000'
+    }
   },
   plugins: [
     vue({
