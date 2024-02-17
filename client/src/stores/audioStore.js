6aa19483e899544c57f40993900fa6d9ec8dc41c
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-02-17 20:53:35 +0100

    Add blobToBase64 function and addAudio method to audioStore.js

--- client/src/stores/audioStore.js
+++ client/src/stores/audioStore.js
@@ -6,12 +6,26 @@ const useAudioStore = defineStore('AudioStore', () => {
   const pwaTitle = ref('AudioArchive (prototype)')
   const audios = ref([])
 
+  const blobToBase64 = async blob => {
+    return new Promise((resolve, reject) => {
+      const reader = new window.FileReader()
+      reader.onload = () => resolve(reader.result)
+      reader.onerror = error => reject(error)
+      reader.readAsDataURL(blob)
+    })
+  }
+
   const fetchAudios = async () => {
     const response = await axios.get('/audioarchive')
     audios.value = response.data
   }
 
-  return { pwaTitle, fetchAudios, audios }
+  const addAudio = async audio => {
+    const response = await axios.post('/audioarchive', audio)
+    audios.value.push(response.data)
+  }
+
+  return { pwaTitle, audios, blobToBase64, fetchAudios, addAudio }
 })
 
 export default useAudioStore
