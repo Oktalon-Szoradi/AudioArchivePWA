0c875f84c174a74c92bc7e3bbc9e4f36a5963bd4
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-03-29 18:48:12 +0100

    Handle non-existent pages (not found pages)

---
+++ client/src/views/PageNotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div class="title">not found</div>
+    <div class="q-mt-xl text-center">
+      <h4>Looking for <b>{{ $route.fullPath }}</b>?</h4>
+      <h5>I ain't got such a page.</h5>
+      <q-btn to="/" glossy push color="primary" label="Go Home" />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped>
+* {
+  color: #3d1a78;
+}
+</style>
