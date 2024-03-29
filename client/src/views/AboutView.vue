657be346c9087b569f0a2c6b2f4bb809a18a64fe
Oktalon-Szoradi <szoradi.o19@htlwienwest.at>
2024-03-29 18:17:06 +0100

    Update fonts credit card

--- client/src/views/AboutView.vue
+++ client/src/views/AboutView.vue
@@ -28,21 +28,45 @@
       </q-card-section>
       <q-separator />
       <q-card-section>
-        Main font: <b>Oxygen</b><br />
-        Copyright &copy; 2012, vernon adams (vern@newtypography.co.uk), with Reserved Font
-        Names 'Oxygen'<br />
-        This Font Software is licensed under the SIL Open Font License, Version 1.1.<br />
-        <a href="/Oxygen_OFL.txt" target="_blank" style="word-break: break-all">
-          Read the license
-        </a>
+        <q-card class="text-center weak-card">
+          <q-card-section> Main font: <b>Oxygen</b> </q-card-section>
+          <q-separator />
+          <q-card-section>
+            Copyright &copy; 2012, vernon adams (vern@newtypography.co.uk), with Reserved
+            Font Names 'Oxygen'<br />
+            This Font Software is licensed under the SIL Open Font License, Version 1.1.
+          </q-card-section>
+          <q-card-section>
+            <a href="/Oxygen_OFL.txt" target="_blank" style="word-break: break-all">
+              Read the license
+            </a>
+            <br />
+            Obtained from
+            <a href="https://fonts.google.com/specimen/Oxygen" target="_blank">
+              Google Fonts
+            </a>
+          </q-card-section>
+        </q-card>
       </q-card-section>
       <q-card-section>
-        Timer/Stopwatch font: <b>E1234</b><br />
-        Copyright &copy; 2020 GGBot, with Reserved Font Name "E1234".<br />
-        This Font Software is licensed under the SIL Open Font License, Version 1.1.<br />
-        <a href="/E1234_License.txt" target="_blank" style="word-break: break-all">
-          Read the license
-        </a>
+        <q-card class="text-center weak-card">
+          <q-card-section> Timer/Stopwatch font: <b>E1234</b> </q-card-section>
+          <q-separator />
+          <q-card-section>
+            Copyright &copy; 2020 GGBot, with Reserved Font Name "E1234".<br />
+            This Font Software is licensed under the SIL Open Font License, Version 1.1.
+          </q-card-section>
+          <q-card-section>
+            <a href="/E1234_License.txt" target="_blank" style="word-break: break-all">
+              Read the license
+            </a>
+            <br />
+            Obtained from
+            <a href="https://www.1001fonts.com/e1234-font.html" target="_blank">
+              1001 Fonts
+            </a>
+          </q-card-section>
+        </q-card>
       </q-card-section>
     </q-card>
     <q-card class="text-center q-ma-md">
@@ -102,5 +126,11 @@ const audioStore = useaudioStore()
 
 .q-card {
   background-color: hsla(211deg 64% 95% / 50%);
+}
+
+.weak-card {
+  box-shadow: 0 2px 4px hsla(211deg 64% 95% / 50%),
+    inset 0 0 8px hsla(211deg 64% 25% / 25%);
+  background-color: hsla(211deg 64% 40% / 15%);
 }
 </style>
